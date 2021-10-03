78d22aa16b7395a0d1b3242f987f31317b63d483
Hubert-Marzecki <hubert.marzecki@gmail.com>
2021-10-03 15:47:49 +0200

    api call

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,19 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  clickCounter : number = 0;
-  name : string = "hello";
+  clickCounter: number = 0;
+  name: string = 'hello';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  countClick() : void {
+  countClick(): void {
     this.clickCounter += 1;
   }
 
+  setClasses() {
+    const classes = {
+      active: this.clickCounter > 4,
+      notActive: this.clickCounter <= 4,
+    };
+    return classes;
+  }
 }
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  characters: any = {
+    info: [],
+    retults: []
   }
 
+
+  constructor(private _http: HttpService) {}
+
+
+  ngOnInit(): void {
+    this._http.getCharacters().subscribe(data => {
+      this.characters = data;
+      console.log(this.characters)
+    })
+  }
 }
