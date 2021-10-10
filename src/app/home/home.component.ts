6513cad6f8923a444e5f226b0895fbc4927c2c7f
Hubert-Marzecki <hubert.marzecki@gmail.com>
2021-10-10 16:15:25 +0200

    Todo finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   clickCounter: number = 0;
   name: string = 'hello';
+  todos: string[] = ["hello", "jedzenie"]
 
   constructor() {}
 
@@ -23,5 +24,10 @@ export class HomeComponent implements OnInit {
       notActive: this.clickCounter <= 4,
     };
     return classes;
+  }
+
+  addTodo(item: string) {
+    this.todos.push(item);
+    this.name = ""
   }
 }
